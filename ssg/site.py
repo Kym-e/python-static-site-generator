fd00a5ed3f04f258396179fe5501a947586d21b7
Kym-e <83183893+Kym-e@users.noreply.github.com>
2021-11-22 20:35:23 +0000

    attempt to resolve syntax error in line 15

--- ssg/site.py
+++ ssg/site.py
@@ -7,7 +7,7 @@ class Site:
 
     def create_dir(self, path):
         directory = self.dest / relative_to(self.source)
-        Path.mkdir(parents = True, exist_ok = True)
+        path.mkdir(parents = True, exist_ok = True)
 
     def build(self):
         self.dest.mkdir(parents=True, exist_ok=True)
