ee5fc7c89dac88c87a6267ef967f8e29f13aa1cb
Kym-e <83183893+Kym-e@users.noreply.github.com>
2021-11-22 20:17:42 +0000

    Create Site class with constructor method, create_dir and build methods

--- ssg/site.py
+++ ssg/site.py
@@ -1,1 +1,16 @@
-import Path from pathlib
+from pathlib import Path
+
+class Site:
+    def __init__(self, source, dest):
+        self.source = Path(source)
+        self.dest = Path(source)
+
+    def create_dir(self, path):
+        directory = self.dest / relative_to(self.source)
+        Path.mkdir(parents = True, exist_ok = True)
+
+    def build(self):
+        self.dest.mkdir(parents=True, exist_ok=True)
+        for path in self.source.rgob("*"):
+            if path is_dir():
+                self.create_dir(path)
